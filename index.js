ade2f99201aa50cb00df39d5d9b6c037757cb31e
enParadigm <enparadigm@enParadigms-MacBook-Air.local>
2020-07-31 16:21:55 +0530

    configured server and database connections

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
-const express = require('express');
+const express = require("express");
+const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
 
 const app = express();
 
-const PORT = process.env.PORT || 8000;
-const server = app.listen(PORT, () => {
-    console.log(`Server is running at port ${PORT}`);
+//  MIDDLEWARES
+app.use(morgan('dev'));
+if(process.env.NODE_ENV === 'development')
+{
+    console.log(process.env);
+}
+const limiter = rateLimit({
+    max: 100,
+    windowMS: 60 * 60 * 1000,
+    message: 'Too many requests from this IP'
+})
+app.use("/api",limiter);
+
+app.use(express.json());
+app.use(express.static(`${__dirname}/public/`));
+
+app.use((req,res,next) => {
+    req.requestTime = new Date().toISOString();
+    // console.log(req.headers);
+
+    next();
+})
+
+//HANDLING UNHANDLED ROUTES
+app.all('*',(req,res,next) => {  //app.all - handle all the http requests including get,post,delete etc
+    next(new appError(`cannot find ${req.originalUrl} on this server`,404));
 });
 
-app.get("/", (req, res) => {
-    res.status(200)
-       .send("welcome...!");
-})
+module.exports = app;
